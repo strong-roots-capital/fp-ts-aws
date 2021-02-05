78047c7b9b88512f4280bb97691642ba3072ea34
Eric Crosson <eric.s.crosson@utexas.edu>
2021-02-04 20:27:30 -0600

    fix: update import paths

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+/**
+ * @since 0.0.1
+ */
+export * as Sqs from './sqs'
---
+++ src/sqs/index.ts
@@ -0,0 +1,9 @@
+/**
+ * @since 0.0.1
+ */
+export * as BatchResultErrorEntry from './BatchResultErrorEntry'
+
+/**
+ * @since 0.0.1
+ */
+export * as SendMessageBatchResult from './SendMessageBatchResult'
